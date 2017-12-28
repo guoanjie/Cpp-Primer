795715ce3ba51057ffc459072170ed50ffa9a8d2
Eric Guo <guoanjie@gmail.com>
2017-12-27 21:57:13 -0500

    reformat 1.22

--- ch01/1.22.cc
+++ ch01/1.22.cc
@@ -2,7 +2,7 @@
 #include "Sales_item.h"
 int main()
 {
-    Sales_item trans, total;
+    Sales_item total, trans;
     while (std::cin >> trans)
         total += trans;
     std::cout << total << std::endl;
