fe71c0bf48cf0a246a738f19358728d24c14b9ec
Eric Guo <guoanjie@gmail.com>
2017-12-30 08:58:06 -0500

    add 2.31

---
+++ ch02/2.31.cc
@@ -0,0 +1,15 @@
+int main()
+{
+    int i;
+    const int v2 = 0;   int v1 = v2;
+    int *p1 = &v1, &r1 = v1;
+    const int *p2 = &v2, *const p3 = &i, &r2 = v2;
+
+    r1 = v2;
+    // p1 = p2;
+    p2 = p1;
+    // p1 = p3;
+    p2 = p3;
+
+    return 0;
+}
