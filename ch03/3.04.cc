2a3eb1a2374d08468c19561ce9899c4bf9e9ef4b
Eric Guo <guoanjie@gmail.com>
2017-12-31 12:59:39 -0500

    add 3.04

---
+++ ch03/3.04.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    if (s < t)
+        cout << t << " is larger" << endl;
+    else if (s > t)
+        cout << s << " is larger" << endl;
+    else
+        cout << s << " and " << t << " are equal" << endl;
+    if (s.length() < t.length())
+        cout << t << " is longer" << endl;
+    else if (s.length() > t.length())
+        cout << s << " is longr" << endl;
+    else
+        cout << s << " and " << t << " have the same length" << endl;
+    return 0;
+}
