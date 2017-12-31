112cf36911fca75c73e9c02bb90991249b613c87
Eric Guo <guoanjie@gmail.com>
2017-12-31 15:23:39 -0500

    add 3.16 and update 3.13

--- ch03/3.16.cc
+++ ch03/3.16.cc
@@ -14,5 +14,12 @@ int main()
     vector<int> v5{10, 42};         //  2: 10 42
     vector<string> v6{10};          // 10: ""
     vector<string> v7{10, "hi"};    // 10: "hi"
+    for (auto e : v1)   cout << e << " ";   cout << endl;
+    for (auto e : v2)   cout << e << " ";   cout << endl;
+    for (auto e : v3)   cout << e << " ";   cout << endl;
+    for (auto e : v4)   cout << e << " ";   cout << endl;
+    for (auto e : v5)   cout << e << " ";   cout << endl;
+    for (auto e : v6)   cout << e << " ";   cout << endl;
+    for (auto e : v7)   cout << e << " ";   cout << endl;
     return 0;
 }
