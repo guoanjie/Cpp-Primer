457eaa3f12cd8e64de6e3db2526a8f96cbad9340
Eric Guo <guoanjie@gmail.com>
2017-12-31 17:43:28 -0500

    add 3.25

---
+++ ch03/3.25.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+int main()
+{
+    vector<unsigned> scores(11, 0);
+    unsigned grade;
+    while (cin >> grade)
+        ++*(scores.begin() + grade / 10);
+    for (auto s : scores)
+        cout << s << " ";
+    cout << endl;
+    return 0;
+}
