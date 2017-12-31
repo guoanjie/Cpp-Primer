d344ff8af95b9085415c7b7aa46d78f1803c01a8
Eric Guo <guoanjie@gmail.com>
2017-12-31 15:02:59 -0500

    add 3.12

---
+++ ch03/3.12.cc
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+int main()
+{
+    vector<vector<int>> ivec;
+    // vector<string> svec = ivec;
+    vector<string> svec(10, "null");
+    return 0;
+}
