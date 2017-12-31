8604cb833196c138a0c98c6e707c495300c58249
Eric Guo <guoanjie@gmail.com>
2017-12-31 12:49:16 -0500

    add 3.02

---
+++ ch03/3.02.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::string;
+int main()
+{
+    string line, word;
+    // while(getline(cin, line))   cout << line << endl;
+    // while(cin >> word)  cout << word << endl;
+    return 0;
+}
