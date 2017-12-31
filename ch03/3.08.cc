0d29a950fd2b2604fa6487916b812fe32885ba40
Eric Guo <guoanjie@gmail.com>
2017-12-31 14:19:12 -0500

    add 3.08

---
+++ ch03/3.08.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+int main()
+{
+    string s;
+    cin >> s;
+    decltype(s.length()) i = 0;
+    while (i < s.length()) {
+        s[i] = 'X';
+        ++i;
+    }
+    cout << s << endl;
+    cin >> s;
+    for (decltype(s.length()) i = 0; i < s.length(); ++i)
+        s[i] = 'X';
+    cout << s << endl;
+    return 0;
+}
