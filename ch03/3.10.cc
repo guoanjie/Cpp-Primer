3ef6c52acb289f41f2434ae9ff224aa8f37f1569
Eric Guo <guoanjie@gmail.com>
2017-12-31 14:30:19 -0500

    add 3.10

---
+++ ch03/3.10.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::getline;
+using std::string;
+int main()
+{
+    string input, output;
+    getline(cin, input);
+    for (auto c: input)
+    {
+        if(!ispunct(c))
+            output += c;
+    }
+    cout << output << endl;
+    return 0;
+}
