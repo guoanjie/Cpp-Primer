4bdb0e9db40be8727e194d2f7b737dd0f0030f8f
Eric Guo <guoanjie@gmail.com>
2017-12-31 19:22:34 -0500

    use != instead of <

--- ch03/3.31.cc
+++ ch03/3.31.cc
@@ -3,7 +3,7 @@ int main()
 {
     constexpr size_t array_size = 10;
     int ia[array_size];
-    for (size_t ix = 0; ix < array_size; ++ix)
+    for (size_t ix = 0; ix != array_size; ++ix)
         ia[ix] = ix;
     return 0;
 }
