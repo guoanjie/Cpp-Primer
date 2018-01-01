d185d1903042b277e24a39d11feac597fee8ab33
Eric Guo <guoanjie@gmail.com>
2018-01-01 10:17:39 -0500

    add 4.04

---
+++ ch04/4.04.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+int main()
+{
+    // ((12 / 3) * 4) + (5 * 15) + ((24 % 4) / 2) = 91
+    cout << 12 / 3 * 4 + 5 * 15 + 24 % 4 / 2 << endl;   // 91
+    return 0;
+}
