a6b0e95da941f59edfe265a32475feceee55f865
Eric Guo <guoanjie@gmail.com>
2017-12-31 19:14:07 -0500

    add 3.30

---
+++ ch03/3.30.cc
@@ -0,0 +1,9 @@
+#include <cstddef>
+int main()
+{
+    constexpr size_t array_size = 10;
+    int ia[array_size];
+    for (size_t ix = 0; ix < array_size; ++ix)
+        ia[ix] = ix;
+    return 0;
+}
