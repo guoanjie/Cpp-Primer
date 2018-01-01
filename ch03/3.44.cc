7959269ce8435f3f68a3fdce330be8f79398666b
Eric Guo <guoanjie@gmail.com>
2017-12-31 23:45:36 -0500

    add 3.44

---
+++ ch03/3.44.cc
@@ -0,0 +1,40 @@
+#include <cstddef>
+#include <iostream>
+#include <iterator>
+using std::cout;
+using std::endl;
+using std::begin;
+using std::end;
+int main()
+{
+    constexpr size_t rowCnt = 3, colCnt = 4;
+    int ia[rowCnt][colCnt];
+    size_t cnt = 0;
+    for (auto &row : ia)
+        for (auto &col : row) {
+            col = cnt;
+            ++cnt;
+        }
+
+    using int_array = int[colCnt];
+
+    for (int_array &row : ia) {
+        for (int col : row)
+            cout << col << " ";
+        cout << endl;
+    }
+
+    for (size_t i = 0; i != rowCnt; ++i) {
+        for (size_t j = 0; j != colCnt; ++j)
+            cout << ia[i][j] << " ";
+        cout << endl;
+    }
+
+    for (int_array *p = begin(ia); p != end(ia); ++p) {
+        for(int *q = begin(*p); q != end(*p); ++q)
+            cout << *q << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
