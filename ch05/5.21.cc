a6400e64b27a0b288cc6d18a8c1920f65494e21f
Eric Guo <guoanjie@gmail.com>
2018-01-02 22:40:14 -0500

    add 5.21

---
+++ ch05/5.21.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+int main()
+{
+    string last = "", s;
+    bool repeated = false;
+    while (cin >> s) {
+        if (!isupper(s[0])) {
+            last = s;
+            continue;
+        }
+        if (s == last) {
+            repeated = true;
+            break;
+        }
+        last = s;
+    }
+    if (repeated)
+        cout << s << endl;
+    else
+        cout << "no word was repeated" << endl;
+    return 0;
+}
