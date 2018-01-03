2afdf1203aacef6913d47e7c1d5eab2d92afc3d1
Eric Guo <guoanjie@gmail.com>
2018-01-02 23:15:27 -0500

    use the right header

--- ch05/5.24.cc
+++ ch05/5.24.cc
@@ -1,14 +1,15 @@
-#include <exception>
 #include <iostream>
+#include <stdexcept>
 using std::cin;
 using std::cout;
 using std::endl;
+using std::runtime_error;
 int main()
 {
     int i, j;
     cin >> i >> j;
     if (j == 0)
-        throw std::runtime_error("division by zero");
+        throw runtime_error("division by zero");
     cout << i / j << endl;
     return 0;
 }
