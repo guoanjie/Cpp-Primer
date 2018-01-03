3fc208394c77292df7005d58074420245ce3e162
Eric Guo <guoanjie@gmail.com>
2018-01-02 20:18:46 -0500

    add 5.09

---
+++ ch05/5.09.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+int main()
+{
+    unsigned aCnt = 0, eCnt = 0, iCnt = 0, oCnt = 0, uCnt = 0;
+    char ch;
+    while (cin >> ch) {
+        if (ch == 'a')
+            ++aCnt;
+        else if (ch == 'e')
+            ++eCnt;
+        else if (ch == 'i')
+            ++iCnt;
+        else if (ch == 'o')
+            ++oCnt;
+        else if (ch == 'u')
+            ++uCnt;
+    }
+    cout << "Number of vowel a: \t" << aCnt << endl
+         << "Number of vowel e: \t" << eCnt << endl
+         << "Number of vowel i: \t" << iCnt << endl
+         << "Number of vowel o: \t" << oCnt << endl
+         << "Number of vowel u: \t" << uCnt << endl;
+    return 0;
+}
