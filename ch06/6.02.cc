537e59191fa16279303fb88eff1126d95ba59503
Eric Guo <guoanjie@gmail.com>
2018-01-03 07:53:10 -0500

    add 6.02

---
+++ ch06/6.02.cc
@@ -0,0 +1,25 @@
+#include <string>
+using std::string;
+
+string f() {
+    string s;
+    // ...
+    return s;
+}
+
+void f2(int i) { /* ... */ }
+
+int calc(int v1, int v2) {
+    int v;
+    // ...
+    return v;
+}
+
+double square(double x) {
+    return x * x;
+}
+
+int main()
+{
+    return 0;
+}
