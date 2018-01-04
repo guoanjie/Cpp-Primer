55898ff7d8702d83b5085ba66af75f9c4a98404c
Eric Guo <guoanjie@gmail.com>
2018-01-03 20:37:46 -0500

    use pointers

--- ch06/6.10.cc
+++ ch06/6.10.cc
@@ -3,16 +3,16 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-void swap(int &a, int &b) {
-    int temp = a;
-    a = b;
-    b = temp;
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 int main()
 {
     int a, b;
     cin >> a >> b;
-    swap(a, b);
+    swap(&a, &b);
     cout << a << '\t' << b << endl;
 }
