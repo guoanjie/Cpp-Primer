4ba2995383120c97243f1ac5911368b0f02cf3ee
Eric Guo <guoanjie@gmail.com>
2018-01-06 16:45:12 -0500

    remove extra ;

--- ch07/7.02.cc
+++ ch07/7.02.cc
@@ -2,7 +2,7 @@
 using std::string;
 
 struct Sales_data {
-    string isbn() const { return bookNo; };
+    string isbn() const { return bookNo; }
     Sales_data &combine(const Sales_data &);
 
     string bookNo;
