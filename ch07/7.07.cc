3bad0a5574d9f11b2bbe2fcd8f73628896a7f60a
Eric Guo <guoanjie@gmail.com>
2018-01-06 17:27:46 -0500

    add 7.07

---
+++ ch07/7.07.cc
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+using std::cerr;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::istream;
+using std::ostream;
+using std::string;
+
+struct Sales_data {
+    string isbn() const { return bookNo; }
+    Sales_data &combine(const Sales_data &);
+    double avg_price() const;
+
+    string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+Sales_data &Sales_data::combine(const Sales_data &rhs) {
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+double Sales_data::avg_price() const {
+    if (units_sold)
+        return revenue / units_sold;
+    else
+        return 0;
+}
+
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs) {
+    Sales_data sum = lhs;
+    sum.combine(rhs);
+    return sum;
+}
+
+istream &read(istream &is, Sales_data &item) {
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = price * item.units_sold;
+    return is;
+}
+
+ostream &print(ostream &os, const Sales_data &item) {
+    os << item.isbn() << " " << item.units_sold << " "
+       << item.revenue << " " << item.avg_price();
+    return os;
+}
+
+int main()
+{
+    Sales_data total;
+    if (read(cin, total)) {
+        Sales_data trans;
+        while (read(cin, trans)) {
+            if (total.isbn() == trans.isbn())
+                total.combine(trans);
+            else {
+                print(cout, total) << endl;
+                total = trans;
+            }
+        }
+        print(cout, total) << endl;
+    } else {
+        cerr << "No data?!" << endl;
+    }
+    return 0;
+}
