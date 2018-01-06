7f0967cefe4cd332f80e9547a163a01f6d4d30c5
Eric Guo <guoanjie@gmail.com>
2018-01-06 18:15:49 -0500

    add 7.12

---
+++ ch07/7.12.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+using std::istream;
+using std::ostream;
+using std::string;
+
+struct Sales_data;
+istream &read(istream &, Sales_data &);
+
+struct Sales_data {
+    Sales_data() = default;
+    Sales_data(const string &s): bookNo(s) { }
+    Sales_data(const string &s, unsigned n, double p):
+            bookNo(s), units_sold(n), revenue(p*n) { }
+    Sales_data(istream &is) {
+        read(is, *this);
+    }
+
+    string isbn() const { return bookNo; }
+    Sales_data &combine(const Sales_data &);
+    double avg_price() const;
+
+    string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+Sales_data &Sales_data::combine(const Sales_data &rhs) {
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+double Sales_data::avg_price() const {
+    if (units_sold)
+        return revenue / units_sold;
+    else
+        return 0;
+}
+
+Sales_data add(const Sales_data &lhs, const Sales_data &rhs) {
+    Sales_data sum = lhs;
+    sum.combine(rhs);
+    return sum;
+}
+
+istream &read(istream &is, Sales_data &item) {
+    double price = 0;
+    is >> item.bookNo >> item.units_sold >> price;
+    item.revenue = price * item.units_sold;
+    return is;
+}
+
+ostream &print(ostream &os, const Sales_data &item) {
+    os << item.isbn() << " " << item.units_sold << " "
+       << item.revenue << " " << item.avg_price();
+    return os;
+}
+
+int main()
+{
+    return 0;
+}
