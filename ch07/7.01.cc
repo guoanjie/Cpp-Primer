6915efbb2987064f07ce29d0337ed894b386bf1e
Eric Guo <guoanjie@gmail.com>
2018-01-06 16:06:18 -0500

    no need to define functions yet

--- ch07/7.01.cc
+++ ch07/7.01.cc
@@ -13,45 +13,30 @@ struct Sales_data {
     string bookNo;
     unsigned units_sold = 0;
     double revenue = 0.0;
-
-    string isbn() const {
-        return bookNo;
-    }
-
-    void combine(const Sales_data& other) {
-        assert(isbn() == other.isbn());
-        units_sold += other.units_sold;
-        revenue += other.revenue;
-    }
 };
-
-istream &read(istream &in, Sales_data &item) {
-    double price;
-    in >> item.bookNo >> item.units_sold >> price;
-    item.revenue = item.units_sold * price;
-    return in;
-}
-
-ostream &print(ostream &out, const Sales_data &item) {
-    out << item.bookNo << " " << item.units_sold << " "
-        << item.revenue << " " << item.revenue / item.units_sold;
-    return out;
-}
 
 int main()
 {
     Sales_data total;
-    if (read(cin, total)) {
+    double price;
+    if (cin >> total.bookNo >> total.units_sold >> price) {
+        total.revenue = total.units_sold * price;
         Sales_data trans;
-        while (read(cin, trans)) {
-            if (total.isbn() == trans.isbn())
-                total.combine(trans);
-            else {
-                print(cout, total) << endl;
+        while (cin >> trans.bookNo >> trans.units_sold >> price) {
+            trans.revenue = trans.units_sold * price;
+            if (total.bookNo == trans.bookNo) {
+                total.units_sold += trans.units_sold;
+                total.revenue += trans.revenue;
+            } else {
+                cout << total.bookNo << " " << total.units_sold << " "
+                     << total.revenue << " " << total.revenue / total.units_sold
+                     << endl;
                 total = trans;
             }
         }
-        print(cout, total) << endl;
+        cout << total.bookNo << " " << total.units_sold << " "
+             << total.revenue << " " << total.revenue / total.units_sold
+             << endl;
     } else {
         cerr << "No data?!" << endl;
     }
