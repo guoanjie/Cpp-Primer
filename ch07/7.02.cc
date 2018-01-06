b6e3060494fe9e1cf497b750e2127633f55db07a
Eric Guo <guoanjie@gmail.com>
2018-01-06 16:42:42 -0500

    add 7.02

---
+++ ch07/7.02.cc
@@ -0,0 +1,22 @@
+#include <string>
+using std::string;
+
+struct Sales_data {
+    string isbn() const { return bookNo; };
+    Sales_data &combine(const Sales_data &);
+
+    string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+Sales_data &Sales_data::combine(const Sales_data &rhs) {
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+int main()
+{
+    return 0;
+}
