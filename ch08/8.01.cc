20491a8d213c0a4ce11465af4770eea92fcf3f15
Eric Guo <guoanjie@gmail.com>
2018-01-08 07:34:55 -0500

    add 8.01

---
+++ ch08/8.01.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+std::istream &read(std::istream &is) {
+    std::string s;
+    while (is >> s)
+        std::cout << s << " ";
+    std::cout << std::endl;
+    is.clear();
+    return is;
+}
+
+int main() {
+    return 0;
+}
