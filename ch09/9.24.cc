d044801e35a5aaf960b8da473238e8ca4aec0da3
Eric Guo <guoanjie@gmail.com>
2018-01-09 07:28:49 -0500

    add 9.24

---
+++ ch09/9.24.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> vi;
+    // std::cout << vi.at(0) << std::endl;
+    // std::cout << vi[0] << std::endl;
+    // std::cout << vi.front() << std::endl;
+    // std::cout << *vi.begin() << std::endl;
+    return 0;
+}
