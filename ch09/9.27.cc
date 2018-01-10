71ae1c92edff4a74ae881d8636b7c587aeaf0fbf
Eric Guo <guoanjie@gmail.com>
2018-01-09 22:26:51 -0500

    add 9.27

---
+++ ch09/9.27.cc
@@ -0,0 +1,20 @@
+#include <forward_list>
+#include <iostream>
+
+int main() {
+    std::forward_list<int> flst = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    auto prev = flst.before_begin();
+    auto curr = flst.begin();
+    while (curr != flst.end()) {
+        if (*curr % 2)
+            curr = flst.erase_after(prev);
+        else {
+            prev = curr;
+            ++curr;
+        }
+    }
+    for (auto i : flst)
+        std::cout << i << " ";
+    std::cout << std::endl;
+    return 0;
+}
