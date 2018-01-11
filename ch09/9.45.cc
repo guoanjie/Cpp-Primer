49bddd7b0de9775fbd467b3dfcebdc4b7eb7ce5f
Eric Guo <guoanjie@gmail.com>
2018-01-10 23:25:04 -0500

    add 9.45

---
+++ ch09/9.45.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+std::string generate(std::string name, std::string prefix, std::string suffix) {    prefix += " ";
+    name.insert(name.begin(), prefix.cbegin(), prefix.cend());
+    name.append(" ");
+    name.append(suffix.cbegin(), suffix.cend());
+    return name;
+}
+
+int main() {
+    std::string name, prefix, suffix;
+    std::cin >> name >> prefix >> suffix;
+    std::cout << generate(name, prefix, suffix) << std::endl;
+    return 0;
+}
