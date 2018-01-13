fa398459332ec6746505cc610b1f0243de2b0376
Eric Guo <guoanjie@gmail.com>
2018-01-13 10:13:25 -0500

    change format

--- ch10/10.16.cc
+++ ch10/10.16.cc
@@ -10,16 +10,11 @@ void elimDups(std::vector<std::string> &words) {
 
 void biggies(std::vector<std::string> &words, std::vector<std::string>::size_type sz) {
     elimDups(words);
-    stable_sort(
-        words.begin(),
-        words.end(),
-        [](const std::string &a, const std::string &b) { return a.size() < b.size(); }
-    );
-    auto wc = find_if(
-        words.begin(),
-        words.end(),
-        [sz](const std::string &a) { return a.size() >= sz; }
-    );
+    stable_sort(words.begin(), words.end(),
+            [](const std::string &a, const std::string &b)
+                { return a.size() < b.size(); });
+    auto wc = find_if(words.begin(), words.end(),
+            [sz](const std::string &a) { return a.size() >= sz; });
     for_each(wc, words.end(), [](const std::string &s){std::cout << s << " ";});
     std::cout << std::endl;
 }
