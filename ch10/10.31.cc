5958e8a18ab05c5f85a79b8f4c9ca3fc30b1f408
Eric Guo <guoanjie@gmail.com>
2018-01-14 17:01:42 -0500

    add 10.31

---
+++ ch10/10.31.cc
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+int main() {
+    std::istream_iterator<int> is_iter(std::cin), eof;
+    std::vector<int> vi(is_iter, eof);
+    std::sort(vi.begin(), vi.end());
+    std::ostream_iterator<int> os_iter(std::cout, " ");
+    std::unique_copy(vi.cbegin(), vi.cend(), os_iter);
+    std::cout << std::endl;
+    return 0;
+}
