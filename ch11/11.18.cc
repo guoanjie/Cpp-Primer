f478078d495277fe40b3b68f34f2b6502edf7393
Eric Guo <guoanjie@gmail.com>
2018-01-15 17:10:13 -0500

    add 11.18

---
+++ ch11/11.18.cc
@@ -0,0 +1,8 @@
+#include <map>
+#include <string>
+
+int main() {
+    std::map<std::string, size_t> word_count;
+    std::map<std::string, size_t>::iterator map_it = word_count.begin();
+    return 0;
+}
