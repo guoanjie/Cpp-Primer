72a7a7c4f7fc1bd4627ffad956f1e3b74e35409e
Eric Guo <guoanjie@gmail.com>
2018-01-15 18:27:44 -0500

    add 11.31

---
+++ ch11/11.31.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+int main() {
+    std::multimap<std::string, std::string> authors = {
+        {"Joyce", "Ulysses"},
+        {"Austen", "Pride Prejudice"},
+        {"Austen", "Emma"},
+        {"Dickens", "Oliver Twist"},
+        {"Dickens", "David Copperfield"},
+        {"Dickens", "A Tale of Two Cities"},
+        {"Barth", "Sot-Weed Factor"},
+        {"Barth", "Lost in the Funhouse"},
+    };
+    for (std::string author; std::cin >> author;)
+        authors.erase(author);
+    for (const auto &p : authors)
+        std::cout << p.first << '\t' << p.second << std::endl;
+    return 0;
+}
