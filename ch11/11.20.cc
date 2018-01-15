154401f0ab0f369748f1b5e216c88fcf33093160
Eric Guo <guoanjie@gmail.com>
2018-01-15 17:27:33 -0500

    add 11.20

---
+++ ch11/11.20.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+int main() {
+    std::map<std::string, size_t> word_count;
+    std::string word;
+    while (std::cin >> word) {
+        auto ret = word_count.insert({word, 1});
+        if (!ret.second)
+            ++ret.first->second;
+    }
+    for (const auto &w : word_count)
+        std::cout << w.first << " occurs " << w.second
+                  << ((w.second > 1) ? " times" : " time") << std::endl;
+    return 0;
+}
