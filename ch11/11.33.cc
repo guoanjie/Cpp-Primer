d907af36e21d2a0efd29df4570efcfe5693ee8e4
Eric Guo <guoanjie@gmail.com>
2018-01-15 19:00:14 -0500

    add 11.33

---
+++ ch11/11.33.cc
@@ -0,0 +1,48 @@
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <sstream>
+
+std::map<std::string, std::string> buildMap(std::ifstream &map_file) {
+    std::map<std::string, std::string> trans_map;
+    std::string key;
+    std::string value;
+    while (map_file >> key && getline(map_file, value))
+        if (value.size() > 1)
+            trans_map[key] = value.substr(1);
+        else
+            throw std::runtime_error("no rule for " + key);
+    return trans_map;
+}
+
+const std::string &transform(const std::string &s, const std::map<std::string, std::string> &m) {
+    auto map_it = m.find(s);
+    if (map_it != m.cend())
+        return map_it->second;
+    else
+        return s;
+}
+
+void word_transform(std::ifstream &map_file, std::ifstream &input) {
+    auto trans_map = buildMap(map_file);
+    std::string text;
+    while (getline(input, text)) {
+        std::istringstream stream(text);
+        std::string word;
+        bool firstword = true;
+        while (stream >> word) {
+            if (firstword)
+                firstword = false;
+            else
+                std::cout << " ";
+            std::cout << transform(word, trans_map);
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    std::ifstream map_file("11.33.dict"), input("11.33.text");
+    word_transform(map_file, input);
+    return 0;
+}
