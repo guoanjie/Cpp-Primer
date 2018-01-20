e43af10d848324530dfa942d4c64178d43946e21
Eric Guo <guoanjie@gmail.com>
2018-01-20 09:43:48 -0500

    add 12.32

---
+++ ch12/12.32.cc
@@ -0,0 +1,177 @@
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+
+class ConstStrBlobPtr;
+
+class StrBlob {
+    friend class ConstStrBlobPtr;
+public:
+    typedef std::vector<std::string>::size_type size_type;
+    StrBlob();
+    StrBlob(std::initializer_list<std::string> il);
+    size_type size() const { return data->size(); }
+    bool empty() const { return data->empty(); }
+    void push_back(const std::string &t) { data->push_back(t); }
+    void pop_back();
+    std::string& front() { return get_front(); }
+    const std::string& front() const { return get_front(); }
+    std::string& back() { return get_back(); }
+    const std::string& back() const { return get_back(); }
+    ConstStrBlobPtr begin() const;
+    ConstStrBlobPtr end() const;
+private:
+    std::shared_ptr<std::vector<std::string>> data;
+    void check(size_type i, const std::string &msg) const;
+    std::string& get_front() const;
+    std::string& get_back() const;
+};
+
+StrBlob::StrBlob(): data(std::make_shared<std::vector<std::string>>()) { }
+
+StrBlob::StrBlob(std::initializer_list<std::string> il) :
+        data(std::make_shared<std::vector<std::string>>(il)) { }
+
+void StrBlob::check(size_type i, const std::string &msg) const {
+    if (i >= data->size())
+        throw std::out_of_range(msg);
+}
+
+std::string& StrBlob::get_front() const {
+    check(0, "front on empty StrBlob");
+    return data->front();
+}
+
+std::string& StrBlob::get_back() const {
+    check(0, "back on empty StrBlob");
+    return data->back();
+}
+
+void StrBlob::pop_back() {
+    check(0, "pop_back on empty StrBlob");
+    data->pop_back();
+}
+
+class ConstStrBlobPtr {
+public:
+    ConstStrBlobPtr(): curr(0) { }
+    ConstStrBlobPtr(const StrBlob &a, size_t sz = 0): wptr(a.data), curr(sz) { }
+    const std::string& deref() const;
+    ConstStrBlobPtr& incr();
+private:
+    std::shared_ptr<std::vector<std::string>>
+        check(std::size_t, const std::string&) const;
+    std::weak_ptr<std::vector<std::string>> wptr;
+    std::size_t curr;
+};
+
+ConstStrBlobPtr StrBlob::begin() const { return ConstStrBlobPtr(*this); }
+ConstStrBlobPtr StrBlob::end() const { return ConstStrBlobPtr(*this, data->size()); }
+
+std::shared_ptr<std::vector<std::string>>
+ConstStrBlobPtr::check(std::size_t i, const std::string &msg) const {
+    auto ret = wptr.lock();
+    if (!ret)
+        throw std::runtime_error("unbound ConstStrBlobPtr");
+    if (i >= ret->size())
+        throw std::out_of_range(msg);
+    return ret;
+}
+
+const std::string& ConstStrBlobPtr::deref() const {
+    auto p = check(curr, "dereference past end");
+    return (*p)[curr];
+}
+
+ConstStrBlobPtr& ConstStrBlobPtr::incr() {
+    check(curr, "increment past end of ConstStrBlobPtr");
+    ++curr;
+    return *this;
+}
+
+class QueryResult;
+
+class TextQuery {
+public:
+    using line_no = StrBlob::size_type;
+    TextQuery(std::ifstream&);
+    QueryResult query(const std::string&) const;
+private:
+    std::shared_ptr<StrBlob> file;
+    std::map<std::string, std::shared_ptr<std::set<line_no>>> wm;
+};
+
+TextQuery::TextQuery(std::ifstream &is): file(new StrBlob) {
+    std::string text;
+    while (getline(is, text)) {
+        file->push_back(text);
+        int n = file->size() - 1;
+        std::istringstream line(text);
+        std::string word;
+        while (line >> word) {
+            auto &lines = wm[word];
+            if (!lines)
+                lines.reset(new std::set<line_no>);
+            lines->insert(n);
+        }
+    }
+}
+
+class QueryResult {
+friend std::ostream& print(std::ostream&, const QueryResult&);
+public:
+    QueryResult(std::string s,
+                std::shared_ptr<std::set<TextQuery::line_no>> p,
+                std::shared_ptr<StrBlob> f):
+            sought(s), lines(p), file(f) { }
+private:
+    std::string sought;
+    std::shared_ptr<std::set<TextQuery::line_no>> lines;
+    std::shared_ptr<StrBlob> file;
+};
+
+QueryResult TextQuery::query(const std::string &sought) const {
+    static std::shared_ptr<std::set<line_no>> nodata(new std::set<line_no>);
+    auto loc = wm.find(sought);
+    if (loc == wm.end())
+        return QueryResult(sought, nodata, file);
+    else
+        return QueryResult(sought, loc->second, file);
+}
+
+std::string make_plural(size_t ctr, const std::string &word, const std::string &ending) {
+    return (ctr > 1) ? word + ending : word;
+}
+
+std::ostream &print(std::ostream &os, const QueryResult &qr) {
+    os << qr.sought << " occurs " << qr.lines->size() << " "
+       << make_plural(qr.lines->size(), "time", "s") << std::endl;
+    for (auto num : *qr.lines) {
+        auto it = qr.file->begin();
+        for (int i = 0; i < num; ++i, it.incr());
+        os << "\t(line " << num + 1 << ") "
+           << it.deref() << std::endl;
+    }
+    return os;
+}
+
+void runQueries(std::ifstream &infile) {
+    TextQuery tq(infile);
+    while (true) {
+        std::cout << "enter word to look for, or q to quit: ";
+        std::string s;
+        if (!(std::cin >> s) || s == "q") break;
+        print(std::cout, tq.query(s)) << std::endl;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    std::ifstream infile(argv[1]);
+    runQueries(infile);
+    return 0;
+}
