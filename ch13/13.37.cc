42d1c63e3fd71d6a3ddd467165102cb078ce758e
Eric Guo <guoanjie@gmail.com>
2018-01-21 22:32:05 -0500

    add 13.37

---
+++ ch13/13.37.cc
@@ -0,0 +1,91 @@
+#include <algorithm>
+#include <set>
+#include <string>
+
+class Folder;
+
+class Message {
+    friend class Folder;
+    friend void swap(Message&, Message&);
+public:
+    explicit Message(const std::string &str = ""):
+            contents(str) { }
+    Message(const Message&);
+    Message& operator=(const Message&);
+    ~Message();
+    void save(Folder&);
+    void remove(Folder&);
+private:
+    std::string contents;
+    std::set<Folder*> folders;
+    void add_to_Folders(const Message&);
+    void remove_from_Folders();
+    void addFdr(Folder *pf) { folders.insert(pf); }
+    void remFdr(Folder *pf) { folders.erase(pf); }
+};
+
+class Folder {
+    friend class Message;
+    friend void swap(Message&, Message&);
+public:
+private:
+    std::set<Message*> messages;
+    void addMsg(Message *pm) { messages.insert(pm); }
+    void remMsg(Message *pm) { messages.erase(pm); }
+};
+
+Message::Message(const Message &m):
+        contents(m.contents), folders(m.folders) {
+    add_to_Folders(m);
+}
+
+Message& Message::operator=(const Message &rhs) {
+    remove_from_Folders();
+    contents = rhs.contents;
+    folders = rhs.folders;
+    add_to_Folders(rhs);
+    return *this;
+}
+
+Message::~Message() {
+    remove_from_Folders();
+}
+
+void Message::save(Folder &f) {
+    addFdr(&f);
+    f.addMsg(this);
+}
+
+void Message::remove(Folder &f) {
+    remFdr(&f);
+    f.remMsg(this);
+}
+
+void Message::add_to_Folders(const Message &m) {
+    for (auto f : m.folders)
+        f->addMsg(this);
+}
+
+void Message::remove_from_Folders() {
+    for (auto f : folders)
+        f->remMsg(this);
+    folders.clear();
+}
+
+void swap(Message &lhs, Message &rhs) {
+    using std::swap;
+    for (auto f : lhs.folders)
+        f->remMsg(&lhs);
+    for (auto f : rhs.folders)
+        f->remMsg(&rhs);
+    swap(lhs.folders, rhs.folders);
+    swap(lhs.contents, rhs.contents);
+    for (auto f : lhs.folders)
+        f->addMsg(&rhs);
+    for (auto f : rhs.folders)
+        f->addMsg(&rhs);
+}
+
+int main() {
+    return 0;
+}
