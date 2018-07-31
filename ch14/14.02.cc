fcb833420a5707bdd83a6218903fb1316afeee30
Eric Guo <guoanjie@gmail.com>
2018-07-31 09:15:12 -0500

    baby re-factor

--- ch14/14.02.cc
+++ ch14/14.02.cc
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 
-struct Sales_data;
-std::istream &read(std::istream &, Sales_data &);
+class Sales_data {
+friend std::istream &read(std::istream &, Sales_data &);
+friend std::ostream &print(std::ostream &, const Sales_data &);
 
-struct Sales_data {
+public:
     Sales_data() = default;
     Sales_data(const std::string &s): bookNo(s) { }
     Sales_data(const std::string &s, unsigned n, double p):
@@ -19,6 +20,7 @@ struct Sales_data {
     Sales_data &combine(const Sales_data &);
     double avg_price() const;
 
+private:
     std::string bookNo;
     unsigned units_sold = 0;
     double revenue = 0.0;
