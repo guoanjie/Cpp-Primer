475678f7b6579a0112852b7f61592653e4d14454
Eric Guo <guoanjie@gmail.com>
2018-08-01 15:40:34 -0500

    add 14.43

---
+++ ch14/14.43.cc
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <vector>
+
+using namespace std::placeholders;
+
+int main() {
+    std::vector<int> primes = {2, 3, 5, 7};
+    int n;
+    while (std::cin >> n) {
+        auto it = std::find_if_not(primes.begin(), primes.end(),
+            // std::bind(std::modulus<int>(), _1, n)
+            [n](const int p) { return n % p; }
+        );
+        std::cout << (it == primes.end() ? 0 : *it) << std::endl;
+    }
+    return 0;
+}
